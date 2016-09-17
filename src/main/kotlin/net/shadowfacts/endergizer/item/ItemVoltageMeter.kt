c452ec63e3f854471d0bbdc87e1a368aaeb05dba
Shadowfacts <me@shadowfacts.net>
2016-09-17 11:59:40 -0400

    Add FU support to the Voltage Meter

--- src/main/kotlin/net/shadowfacts/endergizer/item/ItemVoltageMeter.kt
+++ src/main/kotlin/net/shadowfacts/endergizer/item/ItemVoltageMeter.kt
@@ -12,6 +12,8 @@ import net.minecraft.util.EnumHand
 import net.minecraft.util.math.BlockPos
 import net.minecraft.util.text.TextComponentString
 import net.minecraft.world.World
+import net.minecraftforge.energy.CapabilityEnergy
+import net.minecraftforge.energy.IEnergyStorage
 import net.shadowfacts.shadowmc.ShadowMC
 import net.shadowfacts.shadowmc.item.ItemBase
 
@@ -30,14 +32,16 @@ class ItemVoltageMeter : ItemBase("voltageMeter") {
 	override fun onItemUseFirst(stack: ItemStack, player: EntityPlayer, world: World, pos: BlockPos, facing: EnumFacing, hitX: Float, hitY: Float, hitZ: Float, hand: EnumHand?): EnumActionResult {
 		val te = world.getTileEntity(pos)
 		if (te != null) {
-			var tesla: ITeslaHolder? = null
-			if (te.hasCapability(TeslaCapabilities.CAPABILITY_HOLDER, facing)) {
-				tesla = te.getCapability(TeslaCapabilities.CAPABILITY_HOLDER, facing)
-			} else if (te.hasCapability(TeslaCapabilities.CAPABILITY_HOLDER, null)) {
-				tesla = te.getCapability(TeslaCapabilities.CAPABILITY_HOLDER, null)
-			}
+			val tesla: ITeslaHolder? = if (te.hasCapability(TeslaCapabilities.CAPABILITY_HOLDER, facing)) te.getCapability(TeslaCapabilities.CAPABILITY_HOLDER, facing) else if (te.hasCapability(TeslaCapabilities.CAPABILITY_HOLDER, null)) te.getCapability(TeslaCapabilities.CAPABILITY_HOLDER, null) else null
 			if (tesla != null) {
-				val msg = TextComponentString(I18n.format("item.voltageMeter.msg", tesla.storedPower, tesla.capacity))
+				val msg = TextComponentString(I18n.format("item.voltageMeter.msg", tesla.storedPower, tesla.capacity, "Tesla"))
+				ShadowMC.proxy.sendSpamlessMessage(player, msg, MSG_ID)
+				return EnumActionResult.SUCCESS
+			}
+
+			val fu: IEnergyStorage? = if (te.hasCapability(CapabilityEnergy.ENERGY, facing)) te.getCapability(CapabilityEnergy.ENERGY, facing) else if (te.hasCapability(CapabilityEnergy.ENERGY, null)) te.getCapability(CapabilityEnergy.ENERGY, null) else null
+			if (fu != null) {
+				val msg = TextComponentString(I18n.format("item.voltageMeter.msg", fu.energyStored, fu.maxEnergyStored, ""))
 				ShadowMC.proxy.sendSpamlessMessage(player, msg, MSG_ID)
 				return EnumActionResult.SUCCESS
 			}
